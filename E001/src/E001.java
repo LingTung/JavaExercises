75af319aaa6af94f89031b9e6c40a22ba73440a0
Softmosa <steven@softmosa.com>
2017-07-25 21:58:25 +0800

    add E001

---
+++ E001/src/E001.java
@@ -0,0 +1,25 @@
+/**
+ * 實作時間記錄：
+ * 日期：<mm/dd>: <00:00>~<00:00>: <00:00>
+ * 程式目標：
+ * 設計規格：
+ * 設計想法：
+ * 虛擬碼：
+ *  
+ */
+
+/**
+ * @author ganrqunx
+ *
+ */
+public class E001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
